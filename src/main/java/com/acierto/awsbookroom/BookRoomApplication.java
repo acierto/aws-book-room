45a10a6a7bfc847d37abbb6a736110fad7934622
acierto <bogdannechiporenko@gmail.com>
2020-11-16 21:59:43 +0100

    Initial commit

---
+++ src/main/java/com/acierto/awsbookroom/BookRoomApplication.java
@@ -0,0 +1,11 @@
+package com.acierto.awsbookroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookRoomApplication.class, args);
+    }
+}
